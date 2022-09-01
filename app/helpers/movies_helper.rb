2f86ea027bf4675d37732ba0c6611ef2d3bf8a13
urilixo <falecomelvio@hotmail.com>
2022-09-01 15:56:56 -0300

    Add helper functions to movie

--- app/helpers/movies_helper.rb
+++ app/helpers/movies_helper.rb
@@ -1,2 +1,9 @@
 module MoviesHelper
+    def total_gross(movie)
+        movie.flop? ? "Flop!" : number_to_currency(movie.total_gross, precision: 0)
+    end
+
+    def release_date(movie)
+        movie.released_on.strftime("%B %d, %Y")
+    end
 end
