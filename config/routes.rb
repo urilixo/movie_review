b8881436682ab725be65aa12b80fca79b2e685ff
urilixo <falecomelvio@hotmail.com>
2022-09-02 02:49:26 -0300

    Add route for root and alias for showing movie

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
+  root "movies#index"
   get "movies" => "movies#index"
+  get "movies/:id" => "movies#show", as: "movie"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
