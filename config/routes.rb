036878049cf2d21c8c018899400e37e5c1a346a4
urilixo <falecomelvio@hotmail.com>
2022-09-02 15:18:48 -0300

    Change routes to use resources

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,4 @@
 Rails.application.routes.draw do
   root "movies#index"
-  get "movies" => "movies#index"
-  get "movies/:id" => "movies#show", as: "movie"
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  resources "movies"
 end
