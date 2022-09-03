edaf396b0d9984f4888760ec995521771b09eeb7
urilixo <falecomelvio@hotmail.com>
2022-09-03 02:31:35 -0300

    Add filter queries

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,4 +1,20 @@
 class Movie < ApplicationRecord
+    def self.released
+        where("released_on < ?", Time.now)
+    end
+
+    def self.flop
+        where("total_gross <= ?", 225_000_000)
+    end
+
+    def self.hit
+        where('total_gross >= ?', 300_000_000)
+    end
+
+    def self.recently_added
+        order("created_at desc").limit(3)
+    end
+
     def flop?
         total_gross.blank? || total_gross < 250_000_000
     end
