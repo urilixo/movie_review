292c4f1c2c1f4099bbda63a2158c806c7ba50310
urilixo <falecomelvio@hotmail.com>
2022-09-08 18:56:50 -0300

    Add username field to Users

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,7 @@ class User < ApplicationRecord
   has_secure_password
 
   validates :name, presence: true
+  validates :username, presence: true, uniqueness: {case_sensitive: false}
   validates :email, format: {with: /\S+@\S+/ }, uniqueness: { case_sensitive: false}
 
   def gravatar_id
