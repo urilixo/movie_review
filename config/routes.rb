4d61585600f80b7a4092b62f15f422a133703d91
urilixo <falecomelvio@hotmail.com>
2022-09-12 20:30:41 -0300

    Adapt to rubocop rules

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,13 @@
 Rails.application.routes.draw do
-  resources :favorites
-  root "movies#index"
-  resources "movies" do
+  root 'movies#index'
+  resources 'movies' do
     resources :reviews
+    resources :favorites, only: %i[create destroy]
   end
 
-  resource "session", only: [:new, :create, :destroy]
+  resource 'session', only: %i[new create destroy]
 
   resources :users
-  get "signup" => "users#new"
-  get "signin" => "sessions#new"
+  get 'signup' => 'users#new'
+  get 'signin' => 'sessions#new'
 end
