ba265e9c47589977272d83765f6b86eacbdb9830
urilixo <falecomelvio@hotmail.com>
2022-09-12 22:05:03 -0300

    Add creation and destruction of favorites to controller

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,2 +1,27 @@
 class FavoritesController < ApplicationController
+  before_action :require_signin
+  before_action :set_movie
+  before_action :set_user
+
+  def create
+    @user.favorites.create(movie: @movie)
+    redirect_to @movie
+  end
+
+  def destroy
+    favorite = @user.favorites.find(params[:id])
+    favorite.destroy
+
+    redirect_to @movie, status: :see_other
+  end
+
+  private
+
+  def set_movie
+    @movie = Movie.find(params[:movie_id])
+  end
+
+  def set_user
+    @user = current_user
+  end
 end
