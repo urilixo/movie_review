f1ec70bc4c3a9e4195f5dd2ec7454318c224c6b7
urilixo <falecomelvio@hotmail.com>
2022-09-13 05:06:53 -0300

    Move code into helper method

--- app/helpers/favorites_helper.rb
+++ app/helpers/favorites_helper.rb
@@ -1,2 +1,9 @@
 module FavoritesHelper
+  def favorite_or_unfavorite(movie, favorite)
+    if favorite
+      button_to 'Remove from favorites', movie_favorite_path(movie, favorite), method: :delete
+    else
+      button_to 'Add to favorites', movie_favorites_path(movie)
+    end
+  end
 end
